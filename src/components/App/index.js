1327ace9e821be3def6db87c8311fed612915f08
Marina Sergeyeva <morrrinad@gmail.com>
2023-02-23 00:15:28 +0100

    init commit, eslint prettier confir

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import HomePage from '../../pages/HomePage';
+import Header from '../Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
